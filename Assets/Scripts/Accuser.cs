7902b32051326ab2356c24174286f9c5d9bcbd42
Joshua Mayers <jshuam0@gmail.com>
2020-04-19 09:37:35 +1000

    Finished Accusation for now

--- Assets/Scripts/Accuser.cs
+++ Assets/Scripts/Accuser.cs
@@ -3,7 +3,10 @@
 public class Accuser : MonoBehaviour
 {
     [SerializeField] private GameObject accuseText = null;
+    [SerializeField] private DialogueTrigger dialogueTrigger = null;
     [SerializeField] private Camera mainCamera = null;
+
+    private RaycastHit _hit;
 
     // Start is called before the first frame update
     void Start()
@@ -14,10 +17,15 @@ public class Accuser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(gameObject.transform.position, mainCamera.transform.forward * 2.5f, out hit, 2.5f))
+        CheckForFireStarter();
+        Accuse();
+    }
+
+    private void CheckForFireStarter()
+    {
+        if (Physics.Raycast(gameObject.transform.position, mainCamera.transform.forward * 2.5f, out _hit, 2.5f))
         {
-            if (hit.collider.gameObject.tag == "Employee")
+            if (_hit.collider.gameObject.tag == "Employee")
             {
                 accuseText.SetActive(true);
             }
@@ -26,9 +34,22 @@ public class Accuser : MonoBehaviour
                 accuseText.SetActive(false);
             }
         }
-        if (hit.collider == null)
+        if (_hit.collider == null)
         {
             accuseText.SetActive(false);
+        }
+    }
+
+    private void Accuse()
+    {
+        if (accuseText.activeSelf && Input.GetKeyDown(KeyCode.F))
+        {
+            dialogueTrigger.TriggerDialogue();
+
+            // if (_hit.collider != null && _hit.collider.gameObject.GetComponent<FireStarter>())
+            // {
+            //     _hit.collider.gameObject.GetComponent<DialogueTrigger>().TriggerDialogue();
+            // }
         }
     }
 }
