f72ea258e41b4b6de6f59bebb6368965804d4ca0
Jono <jonathanphame@gmail.com>
2020-04-19 19:35:36 +1000

    Combine employee with fire starting

--- Assets/Scripts/Employee/EmployeeManager.cs
+++ Assets/Scripts/Employee/EmployeeManager.cs
@@ -7,20 +7,18 @@ public class EmployeeManager : MonoBehaviour
 {
     static readonly string[] names = { "Poopy Eyes", "Two Face", "Monster Monster", "Stinky Shrek", "Cancer", "Giga Chad", "Emperor" };
     static readonly string[] roles = { "Master", "Nemesis", "Loyal Crook", "Stinky Crook", "Cancer Crook", "Padawan" };
-    [SerializeField] private int _maxSpeed;
-    [SerializeField] private int _maxWalkWaitTime;
-    [SerializeField] private float _destinationThreshold;
-    [SerializeField] private float _maxRotSpeed;
+    [SerializeField] private int _maxSpeed = 10;
+    [SerializeField] private int _maxWalkWaitTime = 10;
+    [SerializeField] private float _destinationThreshold = 10.0f;
+    [SerializeField] private float _maxRotSpeed = 100.0f;
+    [SerializeField] private Transform _employeesParent = null;
+    [SerializeField] private int spawneeSize;
+    [SerializeField] private List<GameObject> spawnPoints;
+    [SerializeField] private List<GameObject> destinationPoints;
+    [SerializeField] private GameObject spawneePreFab;
 
-    public List<GameObject> checkpoints;
     List<EmployeeController> employees;
-    public Vector3 spawnCentre;
-    public Vector3 spawnSize;
-    public GameObject spawneePreFab;
-    public int spawneeSize;
-
     bool _isInitialised;
-
     System.Random random;
 
     void Start()
@@ -33,7 +31,8 @@ public class EmployeeManager : MonoBehaviour
         employees = new List<EmployeeController>();
         for (var i = 0; i < spawneeSize; i++)
         {
-            SpawnEmployee(randomNames[i % randomNames.Length], randomRoles[i % randomRoles.Length]);
+            var spawnPoint = spawnPoints[random.Next(spawnPoints.Count)];
+            SpawnEmployee(randomNames[i % randomNames.Length], randomRoles[i % randomRoles.Length], spawnPoint.transform.position);
         }
         _isInitialised = true;
     }
@@ -45,17 +44,18 @@ public class EmployeeManager : MonoBehaviour
         {
             if (!employee.IsWalking())
             {
-                var checkpoint = checkpoints[random.Next(checkpoints.Count)];
+                var checkpoint = destinationPoints[random.Next(destinationPoints.Count)];
                 StartCoroutine(employee.MoveTo(checkpoint.transform.position));
             }
         }
     }
-    public void SpawnEmployee(string name, string role)
+
+    public void SpawnEmployee(string name, string role, Vector3 spawnPoint)
     {
-        Vector3 pos = spawnCentre + new Vector3(Random.Range(-spawnSize.x / 2, spawnSize.x / 2), Random.Range(-spawnSize.y / 2, spawnSize.y / 2), Random.Range(-spawnSize.z / 2, spawnSize.z / 2));
-        var employeeGameObj = Instantiate(spawneePreFab, pos, Quaternion.identity);
+        var employeeGameObj = Instantiate(spawneePreFab, spawnPoint, Quaternion.identity, _employeesParent);
         var employeeAgent = employeeGameObj.GetComponent<NavMeshAgent>();
         var employeeScript = employeeGameObj.GetComponent<EmployeeController>();
+
         employeeScript.Initialise(name, role, _maxSpeed, _maxWalkWaitTime, _destinationThreshold, _maxRotSpeed, employeeAgent, Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
         employees.Add(employeeScript);
     }
