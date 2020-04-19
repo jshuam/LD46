b527fd1e6567891c361ef3278e875c1e01d6bf72
Joshua Mayers <jshuam0@gmail.com>
2020-04-19 11:42:01 +1000

    Finished the Fire Starter component
    Signed-off-by: Joshua Mayers <jshuam0@gmail.com>

--- Assets/Scripts/Fire/FireManager.cs
+++ Assets/Scripts/Fire/FireManager.cs
@@ -5,25 +5,27 @@ using System.Linq;
 
 public class FireManager : MonoBehaviour
 {
-    public List<GameObject> flamePrefabs;
+    [SerializeField] private List<GameObject> flamePrefabs = null;
+
+    private System.Random _random;
 
     // Start is called before the first frame update
     void Start()
     {
-        var random = new System.Random();
-        CreateFire( -3, -6, flamePrefabs[random.Next(flamePrefabs.Count)] );
-        CreateFire( 3, -6, flamePrefabs[random.Next(flamePrefabs.Count)] );
+        // CreateFire(-3, -6, flamePrefabs[random.Next(flamePrefabs.Count)]);
+        // CreateFire(3, -6, flamePrefabs[random.Next(flamePrefabs.Count)]);
+        _random = new System.Random();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // Start is called before the first frame update
-    void CreateFire( int x, int z, GameObject flamePrefab )
+    public void CreateFire(float x, float z)
     {
-        Instantiate( flamePrefab, new Vector3( x, 2, z ), Quaternion.identity * Quaternion.Euler (-90f, 0f, 0f) );
+        Instantiate(flamePrefabs[_random.Next(flamePrefabs.Count)], new Vector3(x, 2, z), Quaternion.identity * Quaternion.Euler(-90f, 0f, 0f));
     }
 }
--- Assets/Scripts/FireStarter.cs
+++ Assets/Scripts/FireStarter.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FireStarter : MonoBehaviour
 {
+    private FireManager _fireManager;
+    private System.Random _random;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        _random = new System.Random();
+        _fireManager = FindObjectOfType<FireManager>();
+        InvokeRepeating("RandomFire", 1.0f, 5.0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void RandomFire()
     {
-
+        if (_random.Next(100) > 60)
+        {
+            _fireManager.CreateFire(gameObject.transform.position.x, gameObject.transform.position.z - 1.5f);
+        }
     }
 }
