f18e005211d52ec95d18f252ecb38d5e23700ea8
Francis <souprayer@gmail.com>
2020-04-19 16:53:57 +1000

    Fire damages
    changed bit of healthbar code

--- Assets/Scripts/Fire/FireManager.cs
+++ Assets/Scripts/Fire/FireManager.cs
@@ -9,10 +9,15 @@ public class FireManager : MonoBehaviour
 
     [SerializeField] private GameObject noCollisionFlamePrefab = null;
 
+    [SerializeField] private GameObject healthBar = null;
+
+    private List<GameObject> _fires = new List<GameObject>();
+
 
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("BurnDamage", 0, 1f);
     }
 
     // Update is called once per frame
@@ -20,12 +25,21 @@ public class FireManager : MonoBehaviour
     {
     }
 
+    void BurnDamage()
+    {
+        if ( healthBar != null )
+        {
+            var healthScript = (HealthBar)healthBar.GetComponent( typeof(HealthBar) );
+            healthScript.TakeDamage( (float)_fires.Count );
+        }
+    }
+
     // Start is called before the first frame update
     public void CreateFire(float x, float z, bool collide = true)
     {
         if( collide )
         {
-            Instantiate( flamePrefab, new Vector3(x, 2, z), Quaternion.identity * Quaternion.Euler(-90f, 0f, 0f) );
+            _fires.Add( Instantiate( flamePrefab, new Vector3(x, 2, z), Quaternion.identity * Quaternion.Euler(-90f, 0f, 0f) ) );
         }
         else
         {
--- Assets/Scripts/Life Bar/HealthBar.cs
+++ Assets/Scripts/Life Bar/HealthBar.cs
@@ -8,17 +8,25 @@ public class HealthBar : MonoBehaviour
     private Transform _bar = null;
 
     [SerializeField]
+    private float _barLength;
+
+    [SerializeField]
     private float _health = 0f;
+
+    [SerializeField]
+    public float maxHealth = 100f;
 
     private void Start()
     {
-        _bar.localScale = new Vector3( _health, 1f );
+        _barLength = _bar.localScale.x;
+        setHealth( 100f );
+        _bar.localScale = new Vector3( _barLength * ( _health / maxHealth ), 1f );
     }
 
     // Update is called once per frame
     private void Update()
     {
-        _bar.localScale = new Vector3( _health, 1f );
+        _bar.localScale = new Vector3( _barLength * ( _health / maxHealth ), 1f );
     }
 
     // Set health to specific value
@@ -27,26 +35,22 @@ public class HealthBar : MonoBehaviour
     }
 
     // Subtracts damage as percentage
-    public void TakeDamage(int damage){
-        // the scale of damage to float is 1% = 0.02f;
-        float floatDamage = damage * 0.02f;
-        if(_health + floatDamage > 4f){
-            _health = 4f;
-            return;
-        }
-
-        _health += floatDamage;
-    }
-
-    public void HealDamage(int damage){
-        // the scale of damage to float is 1% = 0.02f;
-        float floatDamage = damage * 0.02f;
-        if(_health - floatDamage < 0f){
+    public void TakeDamage(float damage){
+        if(_health - damage < 0f){
             _health = 0f;
             return;
         }
 
-        _health -= floatDamage;
+        _health -= damage;
+    }
+
+    public void HealDamage(float damage){
+        if(_health + damage > maxHealth){
+            _health = maxHealth;
+            return;
+        }
+
+        _health += damage;
     }
 
 }
