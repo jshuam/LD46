24d5dcf9dfee9d450f7df453ed47b61eab1598ef
Francis <souprayer@gmail.com>
2020-04-19 12:32:43 +1000

    Removes unused flame prefabs

--- Assets/Scripts/Fire/FireManager.cs
+++ Assets/Scripts/Fire/FireManager.cs
@@ -11,8 +11,11 @@ public class FireManager : MonoBehaviour
     void Start()
     {
         var random = new System.Random();
-        CreateFire( -3, -6, flamePrefabs[random.Next(flamePrefabs.Count)] );
-        CreateFire( 3, -6, flamePrefabs[random.Next(flamePrefabs.Count)] );
+        CreateFire( -110f, 0f, flamePrefabs[2] );
+        CreateFire( -110.4f, 0.4f, flamePrefabs[2] );
+        CreateFire( -110.2f, 0f, flamePrefabs[2] );
+        CreateFire( -112.5f, 0f, flamePrefabs[2] );
+        CreateFire( -115f, 0f, flamePrefabs[2] );
     }
 
     // Update is called once per frame
@@ -22,8 +25,8 @@ public class FireManager : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void CreateFire( int x, int z, GameObject flamePrefab )
+    void CreateFire( float x, float z, GameObject flamePrefab )
     {
-        Instantiate( flamePrefab, new Vector3( x, 2, z ), Quaternion.identity * Quaternion.Euler (-90f, 0f, 0f) );
+        Instantiate( flamePrefab, new Vector3( x, 1, z ), Quaternion.identity * Quaternion.Euler (-90f, 0f, 0f) );
     }
 }
