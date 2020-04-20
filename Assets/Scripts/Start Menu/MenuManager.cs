e3f44dc0c564c3a01d8c87b4dd120a0195efff7d
Joshua Mayers <jshuam0@gmail.com>
2020-04-20 15:11:46 +1000

    Added MusicManager & Fixed Flame Prefab

---
+++ Assets/Scripts/Start Menu/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartMainMenuScene()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+
+    public void StartGameScene()
+    {
+        Destroy(FindObjectOfType<MusicManager>().gameObject);
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void StartHowToPlayScene()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
+    }
+}
